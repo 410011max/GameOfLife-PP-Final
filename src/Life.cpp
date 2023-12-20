4811e840cf4ae8c71ed359da73ccc6e23ec85c76
nmyuchen <mxicyuchen@gmail.com>
2023-12-20 16:50:18 +0800

    update openmp version

--- src/Life.cpp
+++ src/Life.cpp
@@ -1,25 +1,30 @@
 #include "Life.h"
-
+#include <omp.h>
 
 void Life::update()
 {
-
-	for (unsigned int i = 1; i <= height; i++)
+	omp_set_num_threads(8);
+	#pragma omp parallel
 	{
-		for (unsigned int j = 1; j <= width; j++)
+		
+		#pragma omp for collapse(2) nowait
+		for (unsigned int i = 1; i <= height; i++)
 		{
-			int m = getNeighbors(j, i, 1);
-			if (m == 3)
+			for (unsigned int j = 1; j <= width; j++)
 			{
-				setNewLife(j, i, 1);
-			}
-			if (m == 2)
-			{
-				setNewLife(j, i, getLifeform(j, i));
-			}
-			if (m != 3 && m != 2)
-			{
-				setNewLife(j, i, 0);
+				int m = getNeighbors(j, i, 1);
+				if (m == 3)
+				{
+					setNewLife(j, i, 1);
+				}
+				if (m == 2)
+				{
+					setNewLife(j, i, getLifeform(j, i));
+				}
+				if (m != 3 && m != 2)
+				{
+					setNewLife(j, i, 0);
+				}
 			}
 		}
 	}
